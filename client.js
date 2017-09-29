a67e3c99b643eb1f336e5fdb1be3ec38624a8367
Asaf Nachmany <asaf.nachmany@tradeshift.com>
2017-09-28 19:40:49 -0700

    First push. AngularJS app that calls a NodeJS server

---
+++ client.js
@@ -0,0 +1,91 @@
+'use strict';
+
+angular.module('TradeshiftPulseApp', [])
+	.constant('API_DOMAIN', 'http://localhost:3000')
+	.constant('API_POLL_URL', '/data')
+	.constant('POLL_TIMEOUT', 1000)
+	.constant('MAP_WIDTH', 1280) // SVG
+	.constant('MAP_HEIGHT', 800) // SVG
+	.service(   'AppService', [
+			    'API_DOMAIN', 'API_POLL_URL', '$http',
+		function(API_DOMAIN,   API_POLL_URL,   $http) {
+
+		var buildURL = function(from, to) {
+			return `${API_DOMAIN}${API_POLL_URL}/from/${from.toISOString()}/to/${to.toISOString()}`;
+		};
+
+		/*
+		 * poll for data collected between {@fromDate} until {@toDate} (default: now)
+		 */
+		this.poll = function(fromDate, toDate) {
+			if (!fromDate) {
+				throw "Missing parameter 'fromDate'";
+			}
+
+			fromDate = new Date(fromDate);
+			if (!toDate) {
+				toDate = new Date();
+			}
+
+			return $http.get(buildURL(fromDate, toDate));
+		};
+	}])
+	.service('PixiService', ['MAP_WIDTH', 'MAP_HEIGHT', function(MAP_WIDTH, MAP_HEIGHT){
+
+		var countryToPixiCountry = function(country) {
+			return {
+				iso: country.iso,
+				lat: country.lat,
+				lon: country.lon,
+				x: ((MAP_WIDTH / 360) * (180 + country.lon)),
+				y: ((MAP_HEIGHT / 180) * (90 - country.lat)),
+			};
+		};
+
+		var eventToPixiEvent = function(event) {
+			return {
+				source: countryToPixiCountry(event.source),
+				dest: countryToPixiCountry(event.dest),
+				volume: event.volume
+			};
+		};
+
+		this.publish = function(events) {
+			var pixiEvents = [];
+			for (var i = 0; i < events.length; i++) {
+				pixiEvents.push(eventToPixiEvent(events[i]));
+			}
+
+			// TODO: display events on PixiJS (asaf/jim)
+			return pixiEvents;
+		};
+
+	}])
+	.controller('AppController', [
+				'POLL_TIMEOUT', 'PixiService', 'AppService', '$scope',
+		function(POLL_TIMEOUT,   PixiService,   AppService,    $scope) {
+
+			var lastTimeStamp = new Date();
+
+			$scope.error = function(msg) {
+				$scope.errorMessage = msg;
+			};
+
+			$scope.poll = function(){
+				setTimeout(function(){
+					AppService.poll(lastTimeStamp).then(function(response) {
+						lastTimeStamp = response.data.end_time; // end of time range already visualized
+						$scope.template_path = PixiService.publish(response.data.events);
+						setTimeout($scope.poll, POLL_TIMEOUT); // go again
+					}).catch(function(error) {
+						$scope.error(error);
+					});
+				}, POLL_TIMEOUT);
+			};
+
+			// init function
+			(function(){
+				$scope.poll();
+			})();
+		}
+	]);
---
+++ server.js
@@ -0,0 +1,35 @@
+var angularserver = require('angularjs-server');
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var request = require('request')
+
+var templateFile = path.join(__dirname, 'index.html');
+var template = fs.readFileSync(templateFile, {encoding:'utf8'});
+var staticDir = __dirname;
+var app = express();
+angularserver.Server({
+    template: template,
+    serverScripts: [
+        path.join(__dirname, 'node_modules/angular/angular.js'),
+        path.join(__dirname, 'client.js')
+    ],
+    clientScripts: [
+        '/:static/node_modules/angular/angular.js',
+        '/:static/node_modules/angular-route/angular-route.js',
+        '/:static/client.js'
+    ],
+    angularModules: [
+        'TradeshiftPulseApp'
+    ]
+});
+
+// Routes
+app.use('/', express.static(staticDir));
+app.use('/data/from/:start_time/to/:end_time', function(req, res) {
+    req.pipe(request(`http://10.128.10.248:8080/events?lastseen=${req.params.start_time}`)).pipe(res)
+});
+
+// Run
+app.listen(3000);
+console.log('listening on port 3000');
